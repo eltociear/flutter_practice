e58532ec0c0f032cd4354bf74ece8ab43ee7bb13
Ikko Ashimine <ashimine_ikko_bp@tenso.com>
2024-06-28 03:11:28 +0900

    hello_world: add icons

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -45,6 +45,26 @@ class _MyHomePageState extends State<MyHomePage> {
         const Text("ハローワールド！"),
         TextButton(onPressed: () => {print("ボタンが押されたよ")},
         child: const Text("ボタン"),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: const [
+            Icon(
+              Icons.favorite,
+              color: Colors.pink,
+              size: 24.0,
+            ),
+            Icon(
+              Icons.audiotrack,
+              color: Colors.green,
+              size: 30.0,
+            ),
+            Icon(
+              Icons.beach_access,
+              color: Colors.blue,
+              size: 36.0,
+            )
+          ],
         )
       ])
     );
