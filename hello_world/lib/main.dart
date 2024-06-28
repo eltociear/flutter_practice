1087cedac210e94b4eea0751a5b820e37c473080
Ikko Ashimine <ashimine_ikko_bp@tenso.com>
2024-06-28 11:00:28 +0900

    hello_world: add floating button

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -66,7 +66,10 @@ class _MyHomePageState extends State<MyHomePage> {
             )
           ],
         )
-      ])
+      ]),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {print("押したね？")}, child: const Icon
+        (Icons.timer)),
     );
   }
 }
