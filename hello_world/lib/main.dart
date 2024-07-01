f4ac28838e9f63d52acaa607c2041197379f48cb
Ikko Ashimine <ashimine_ikko_bp@tenso.com>
2024-07-01 18:47:33 +0900

    test

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
-import 'test_page1.dart';
-import 'test_page2.dart';
-import 'test_page3.dart';
 
+// 1. エントリーポイントのmain関数
 void main() {
+  // 2. MyAppを呼び出す
   runApp(const MyApp());
 }
 
+// MyAppのクラス
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // 3. タイトルとテーマを設定する。 画面の本体はMyHomePageで作る。
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -24,7 +25,8 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({super.key, required this.title});
+
   final String title;
 
   @override
@@ -32,38 +34,47 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late PageController _pageController;
-  int _selectedIndex = 0;
-  // ページの配列
-  final _pages = [
-    TestPage1(),
-    TestPage2(),
-    TestPage3(),
-  ];
-  @override
-  void initState() {
-    super.initState();
-    _pageController = PageController(initialPage: _selectedIndex);
-  }
+  int _counter = 0;
 
-  @override
-  void dispose() {
-    super.dispose();
-    _pageController.dispose();
-  }
-
-  void _onPageChanged(int index) {
+  // 5. カウンタが押された時のメソッド
+  void _incrementCounter() {
     setState(() {
-      _selectedIndex = index;
+      _counter++;
+      print("hello world");
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    // 4. MyHomePageの画面を構築する部分
     return Scaffold(
-        body: PageView(
-            controller: _pageController,
-            onPageChanged: _onPageChanged,
-            children: _pages));
+      // 画面上部のタイトル部分
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // 画面の中央に表示されるテキスト
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            // テキストの下に表示されるカウンタ値
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      // 右下の 「+」 ボタンに対応するフローティングアクションボタン
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
   }
 }
